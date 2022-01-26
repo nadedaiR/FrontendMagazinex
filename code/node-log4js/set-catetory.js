3c59ba1a5ef623ecc900f4a820cdfa5eb7e2c35b
nadedaiR <bbnsy362@163.com>
2022-01-26 16:12:32 +0800

    remove code

--- code/node-log4js/set-catetory.js
+++
@@ -1,4 +0,0 @@
-// file: set-catetory.js
-var log4js = require('log4js');
-var logger = log4js.getLogger('set-catetory.js');
-logger.debug("Time:", new Date());
--- code/node-log4js/simplest.js
+++
@@ -1,4 +0,0 @@
-// file: simplest.js
-var log4js = require('log4js');
-var logger = log4js.getLogger();
-logger.debug("Time:", new Date());
